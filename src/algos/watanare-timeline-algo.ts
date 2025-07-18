bf03af519e98de7ed66b2c3c682666a6fac77b2a
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2025-07-19 02:31:17 +0900

    feat: Add WatanareTimelineAlgo concrete class
    Introduced 'src/algos/watanare-timeline-algo.ts', a concrete implementation
    of the 'Algo' interface. This class provides a feed generation algorithm
    for the 'watanare-timeline' by fetching posts from the 'watanare_post' table.

---
+++ src/algos/watanare-timeline-algo.ts
@@ -0,0 +1,33 @@
+import { AppContext } from '../config';
+import { OutputSchema } from '../lexicon/types/app/bsky/feed/getFeedSkeleton';
+
+import { Algo } from "./algo";
+
+export class WatanareTimelineAlgo implements Algo {
+    shortname: string = 'watanare-timeline'
+    get = async (context: AppContext, params: any): Promise<OutputSchema> => {
+        const builder = context.db
+            .selectFrom('watanare_post')
+            .selectAll()
+            .orderBy('indexedAt', 'desc')
+            .orderBy('cid', 'desc')
+            .limit(params.limit)
+
+        const res = await builder.execute()
+
+        const feed = res.map((row) => ({
+            post: row.uri,
+        }))
+
+        let cursor: string | undefined
+        const last = res.at(-1)
+        if (last) {
+            cursor = new Date(last.indexedAt).getTime().toString(10)
+        }
+
+        return {
+            cursor,
+            feed,
+        }
+    }
+}
